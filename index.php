c831ceb6ba300db3a2062d95a18424062634b30e
kleverck <33034229+kleverck@users.noreply.github.com>
2017-10-23 19:12:46 +0200

    modificado index
    actualizado el nombre de la aplicacion

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ and open the template in the editor.
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Amazzon</title>
+        <title>Amazzon_Editacion</title>
     </head>
     <body>
            
